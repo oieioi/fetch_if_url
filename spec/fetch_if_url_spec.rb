3dd56ffe8223f042098f68fc6c1481d7ac93eff3
oieioi <atsuinatsu.samuifuyu@gmail.com>
2019-06-27 22:36:47 +0900

    rubocop

--- spec/fetch_if_url_spec.rb
+++ spec/fetch_if_url_spec.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 RSpec.describe FetchIfUrl do
-  it "has a version number" do
+  it 'has a version number' do
     expect(FetchIfUrl::VERSION).not_to be nil
   end
 
   it 'fetch a file if its path looks like URL' do
-    expect(FetchIfUrl.fetch_if_url('http://example.com/index.html', dest: "/tmp")).to include "/tmp"
+    expect(FetchIfUrl.fetch_if_url('http://example.com/index.html', dest: '/tmp')).to include '/tmp'
   end
 
   it 'return local path if its looks like local path' do
-    expect(FetchIfUrl.fetch_if_url('assets/test_file', dest: "../tmp")).to eq "assets/test_file"
+    expect(FetchIfUrl.fetch_if_url('assets/test_file', dest: '../tmp')).to eq 'assets/test_file'
   end
 end
