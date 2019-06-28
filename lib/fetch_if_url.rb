69b77870eed9c3a247194888648f871975b159f5
oieioi <atsuinatsu.samuifuyu@gmail.com>
2019-06-28 13:35:53 +0900

    Refactor

--- lib/fetch_if_url.rb
+++ lib/fetch_if_url.rb
@@ -7,26 +7,32 @@ require 'securerandom'
 module FetchIfUrl
   class Error < StandardError; end
 
+  # Fetch a file and return its temporary local path if <code>path</code> looks like URL.
+  # If it doesn't look like URL return as it be.
+  # @param [String] path file path or URL
+  # @param [Hash] opts
+  # @option opts [String] :dest Destination path when fetching
+  # @return [String] local path
   def fetch_if_url(path, dest: '/tmp')
-    if on_network?(path)
-      download(path, dest)
+    if url?(path)
+      fetch(path, dest)
     else
       # do nothing if local file
       path
     end
   end
 
-  def on_network?(path)
+  def url?(path)
     path.start_with?(%r{https?://})
   end
 
-  def download(url, dest)
+  def fetch(url, dest)
     body = Net::HTTP.get(URI.parse(url))
     file_name = "#{dest}/#{Time.now.to_i}-#{SecureRandom.uuid}"
     File.open(file_name, 'wb') { |file| file.write(body) }
     file_name
   end
 
-  module_function :fetch_if_url, :on_network?, :download
-  private_class_method :on_network?, :download
+  module_function :fetch_if_url, :url?, :fetch
+  private_class_method :url?, :fetch
 end
